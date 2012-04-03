e64f9ddc033213743c275c20ca6bc0982b2c6c75
ratno <ratno@knoqdown.com>
2012-04-03 13:32:26 +0700

    js: opsi berantai

---
+++ js/opsi_berantai.js
@@ -0,0 +1,19 @@
+function opsi_berantai(id_selectbox_asal,id_selectbox_tujuan,url_sumber_data,get_param){
+	get_param = (get_param)?get_param:'id'; // jika get_param ga diset maka otomatis terisi id
+	$('#'+id_selectbox_asal).on('change',function(el){
+		$(this).find('option').each(function(i,el){
+			if($(this).attr('selected') == "selected"){
+				$.ajax({
+					type: 'GET',
+					url: url_sumber_data+'?'+get_param+'='+$(this).attr('value'),
+					success: function(data) {
+						$('#'+id_selectbox_tujuan).html(data);
+					},
+					error: function(XMLHttpRequest,textStatus,errorThrown){
+						$('#'+id_selectbox_tujuan).html('Data '+errorThrown);
+					}
+				});
+			}
+		});
+	});
+}
