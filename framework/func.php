87d9e3a5776490767fd5a43c0f3e1f6efecca8b3
ratno <ratno@knoqdown.com>
2012-04-06 11:35:03 +0700

    add textarea using tinymce

--- framework/func.php
+++ framework/func.php
@@ -345,3 +345,39 @@ function is_empty($data) {
   
   return $blnToReturn;
 }
+
+function textarea($name,$data="",$blnReturn=false,$class='tinymce'){
+  if (is_array($data) && isset($data[$name])) {
+    $isi = $data[$name];
+  } else {
+    $isi = $data;
+  }
+  
+  if($class == 'tinymce'){
+    $GLOBALS['js'][] = 'jquery.min.js';
+    $GLOBALS['js'][] = 'tiny_mce/jquery.tinymce.js';
+    $GLOBALS['script'][] = '
+      $("textarea.tinymce").tinymce({
+        script_url : js_url+"tiny_mce/tiny_mce.js",
+        // General options
+        theme : "advanced",
+        plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true
+      });';
+  }
+  $out = "<textarea id='inputbox_".$name."' name='".$name."' class='".$class."'>".$data."</textarea>";
+  if($blnReturn){
+    return $out;
+  } else {
+    echo $out;
+  }
+}
