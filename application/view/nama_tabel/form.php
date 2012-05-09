881ac464d5f682d8362e2f5162b96dc104b77cee
ratno <ratno@knoqdown.com>
2012-05-09 16:45:38 +0700

    default form.php, contoh komponen input cuman satu, sisanya taruh di komentar

--- application/view/nama_tabel/form.php
+++ application/view/nama_tabel/form.php
@@ -1,33 +1,33 @@
+<?php
+/* komponen form yg dapat digunakan:
+ * 1. textbox: 
+ *    <?php input("nama_kolom", $data) ?>
+ * 
+ * 2. file upload: 
+ *    <?php input("nama_kolom", $data, "file") ?>
+ * 
+ * 3. selectbox (pilihan):
+ *    - opsi manual dengan array:
+ *      <?php $opsi_manual = array("Opsi 1"=>"Opsi 1", "Opsi 2"=> "Opsi 2"); ?>
+ *      <?php pilihan("nama_kolom", $opsi_manual, $data) ?>
+ * 
+ *    - opsi dari database :
+ *      <?php $opsi_db = opsi($this->model_XXX->ambil_data(),"nama_kolom_tabel_XXX"); ?> 
+ *      <?php pilihan("nama_kolom", $opsi_db, $data) ?>
+ * 
+ * 4. textarea:
+ *    <?php textarea("nama_kolom", $data) ?>
+ */
+?>
+
 <h1><?php echo $judul ?></h1>
 <form <?php form_properties("nama_tabel", $aksi, $data['id'], true) ?>>
   <table border="0">
 
-
     <tr>
-      <td>Nama Isian Kolom</td>
-      <td><?php input("kolom_di_dalam_tabel", $data) ?></td>
+      <td>Nama Inputan/Isian Form</td>
+      <td><?php input("nama_kolom", $data) ?></td>
     </tr>
-
-
-    <tr>
-      <td>Nama Isian Kolom Untuk Upload</td>
-      <td><?php input("kolom_di_dalam_tabel", $data, "file") ?></td>
-    </tr>
-
-
-    <tr>
-      <td>Nama Isian Kolom Untuk Pilihan Yang Mengambil data dari tabel</td>
-      <td>
-        <?php pilihan("kolom_di_dalam_tabel", $opsi_db, $data) ?>
-      </td>	
-    </tr>
-    <tr>
-      <td>Nama Isian Kolom Untuk Pilihan</td>
-      <td>
-        <?php pilihan("kolom_di_dalam_tabel", $opsi_manual, $data) ?>
-      </td>	
-    </tr>
-
 
     <tr>
       <td colspan="2"><?php submit(); ?></td>
