9a0de6d73ffc09a5e6d9e7b43dace8190e4953fb
ratno <ratno@knoqdown.com>
2012-05-11 22:28:18 +0700

    layout.php, menambahkan pengecekan span klo ga ada maka langsung ambil a(nchor)-nya

--- application/view/layout.php
+++ application/view/layout.php
@@ -25,8 +25,9 @@
         $menu.children('li').each(function(){
           var $this = $(this);
           var $span = $this.children('span');
+          if($span.length == 0) $span = $this.children('a');
           $span.data('width',$span.width());
-					
+
           $this.bind('mouseenter',function(){
             $menu.find('.ldd_submenu').stop(true,true).hide();
             $span.stop().animate({'width':'120px'},300,function(){
