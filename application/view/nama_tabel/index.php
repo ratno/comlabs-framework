8d3f8781c68a58fb310f8efd5f0cbd6124146d5f
ratno <ratno@knoqdown.com>
2012-05-13 15:56:07 +0700

    view/nama_tabel/index.php, menambahkan informasi penggunaan datagrid tabel

--- application/view/nama_tabel/index.php
+++ application/view/nama_tabel/index.php
@@ -1,11 +1,36 @@
+<?php
+/* Komponen yg digunakan untuk tabel yaitu:
+ * "Judul Kolom" 
+ *   => ini adalah judul kolom yang akan muncul pada baris teratas sebuah tabel
+ *   => boleh menggunakan petik dua (") maupun petik satu (')
+ * 
+ * '$item["nama_kolom_dlm_database"]'
+ *   => ini adalah referensi nama kolom yg ada dalam tabel pada database
+ *   => penggunaannya petik paling luar harus petik satu (')
+ *   => misal, pada tabel user ada kolom nama, maka penggunaannya
+ *   ===> '$item["nama"]' <===
+ * 
+ *   => untuk beberapa kebutuhan kita bisa memanfaatkan atau membuat fungsi sendiri
+ *      beberapa contoh penggunaan fungsi:
+ *      - untuk mencetak angka dalam format ribuan, bisa digunakan: angka()
+ *        contoh: 'angka($item["harga"])'
+ *      - untuk mencetak tautan/link (biasanya untuk foto atau file), bisa digunakan: link_href()
+ *        contoh: 'link_href($item["file"])'
+ *      - untuk menampilkan gambar (biasanya untuk foto), bisa digunakan: gambar()
+ *        contoh: 'gambar($item["file"],FILES_URL,200)'
+ *        keterangan: 
+ *        - hasil uploadan biasanya disimpan di folder files (dalam contoh diwakili oleh FILES_URL)
+ *        - untuk ukuran width, bisa diatur dengan memasukkan parameter ketiga (dalam contoh diwakili oleh 200)
+ * 
+ *      
+ * 
+ * 
+ */
+?>
 <h1><?php echo $judul ?></h1>
 <?php echo $link_tambah ?>
 <?php
 tabel("nama_tabel",$data,array(
-    "Judul Kolom 1"=>'$item["nama_kolom1_di_tabel"]',
-    "Judul Kolom 2"=>'angka($item["nama_kolom2_di_tabel"])', // jika mau di format dg pemisah ribuan gunakan fungsi angka()
-    "Judul Kolom 3"=>'link_href($item["nama_kolom3_di_tabel"])', // jika mau menampilkan link dari data yg disimpan dikolom (biasanya file upload)
-    "Judul Kolom 4"=>'gambar($item["nama_kolom4_di_tabel"],FILES_URL)', // jika mau menampilkan gambar dari data yg disimpan dikolom (biasanya file upload berupa gambar)
-    
+    "Judul Kolom"=>'$item["nama_kolom"]',
 ),$aksi);
 ?>
