751d9a275efda7b905ba0b827a4259fb5d161e10
ratno <ratno@knoqdown.com>
2012-05-13 14:23:43 +0700

    layout.php, menyederhanakan menu pada layout dengan menghapus css dan script dari menu

--- application/view/layout.php
+++ application/view/layout.php
@@ -1,7 +1,6 @@
 <html>
   <head>
     <title><?php echo $judul ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo CSS_URL ?>/menu.css" />
     <?php echo $css ?>
   </head>
   <body>
@@ -10,35 +9,5 @@
     <?php echo $main_content ?>
     <?php echo $js ?>
     <?php echo $script ?>
-    <script type="text/javascript">
-      $(function() {
-        /**
-         * the menu
-         */
-        var $menu = $('#ldd_menu');
-				
-        /**
-         * for each list element,
-         * we show the submenu when hovering and
-         * expand the span element (title) to 510px
-         */
-        $menu.children('li').each(function(){
-          var $this = $(this);
-          var $span = $this.children('span');
-          if($span.length == 0) $span = $this.children('a');
-          $span.data('width',$span.width());
-
-          $this.bind('mouseenter',function(){
-            $menu.find('.ldd_submenu').stop(true,true).hide();
-            $span.stop().animate({'width':'120px'},300,function(){
-              $this.find('.ldd_submenu').slideDown(300);
-            });
-          }).bind('mouseleave',function(){
-            $this.find('.ldd_submenu').stop(true,true).hide();
-            $span.stop().animate({'width':$span.data('width')+'px'},300);
-          });
-        });
-      });
-    </script>
   </body>
 </html>
