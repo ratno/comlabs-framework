80ae332cef2d1c0e5c50b82cd32e97304bd5b382
ratno <ratno@knoqdown.com>
2012-05-14 17:23:54 +0700

    set drop table user and page if exists

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,6 @@
 -- CREATE DATABASE cl_framework;
 -- USE cl_framework;
-
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
 	id int primary key auto_increment,
 	nama varchar(100),
@@ -15,6 +15,7 @@ CREATE TABLE user (
 INSERT INTO user (nama, email, hp, username, password, role) VALUES ("Ratno Putro Sulistiyono [Admin]","ratno@comlabs.itb.ac.id","0817201101","ratno","ratno","admin");
 INSERT INTO user (nama, email, username, password, role) VALUES ("Ratno [User]","ratno@knoqdown.com","user","user","user");
 
+DROP TABLE IF EXISTS page;
 CREATE TABLE page (
   id int primary key auto_increment,
   kode varchar(100) unique,
