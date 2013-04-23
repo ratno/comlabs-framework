a204362d7ae903e7b1bca8b36932fcb89af7a467
ratno <ratno@knoqdown.com>
2013-04-24 01:11:24 +0700

    tambahkan css pada layout laporan

--- application/view/laporan.php
+++ application/view/laporan.php
@@ -1,8 +1,9 @@
 <html>
   <head>
     <title><?php echo $judul ?></title>
+    <?php echo $css ?>
   </head>
-  <body>
+  <body style="padding: 10px;">
     <?php echo $main_content ?>
   </body>
 </html>
